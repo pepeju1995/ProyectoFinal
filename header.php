f491f6797ea4e0cca21dc0d21373e57bde41e1a8
pepeju1995 <pepeju95@gmail.com>
2021-09-23 13:28:46 +0200

    implementando vista login

--- header.php
+++ header.php
@@ -1,37 +1,30 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <title>Cabezera</title>
-        <meta charset="utf-8">
-    </head>
 
-    <body>
-        <header>
-            <h1>Soluciones Integrales S.L.</h1>
-            <ul class="nav">
-                <li> <a href="./index.php">Inicio</a></li>
+<header>
+    <h1>Soluciones Integrales S.L.</h1>
+    <div class="menu">
+        <ul class="nav">
+            <li> <a href="./index.php">Inicio</a></li>
 
-                <li><a href="./aseguradoras.php">Aseguradora</a>
-                    <ul>
-                        <li><a href="">Nueva Aseguradora</a></li>
-                        <li><a href="">Mostrar Aseguradoras</a></li>
-                    </ul>
-                </li>
+            <li><a href="./aseguradoras.php">Aseguradora</a>
+                <ul>
+                    <li><a href="">Nueva Aseguradora</a></li>
+                    <li><a href="">Mostrar Aseguradoras</a></li>
+                </ul>
+            </li>
 
-                <li><a href="">Asegurado</a>
-                    <ul>
-                        <li><a href="">Nuevo Asegurado</a></li>
-                        <li><a href="">Mostrar Asegurados</a></li>
-                    </ul>
-                </li>
+            <li><a href="">Asegurado</a>
+                <ul>
+                    <li><a href="">Nuevo Asegurado</a></li>
+                    <li><a href="">Mostrar Asegurados</a></li>
+                </ul>
+            </li>
 
-                <li><a href="">Averias</a>
-                    <ul>
-                        <li><a href="">Nueva Averia</a></li>
-                        <li><a href="">Mostrar Averias</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </header>
-    </body>
-</html>
+            <li><a href="">Averias</a>
+                <ul>
+                    <li><a href="">Nueva Averia</a></li>
+                    <li><a href="">Mostrar Averias</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -7,36 +7,8 @@
     </head>
     <body>
         <?php
-            include "header.php"
+            require_once "header.php";
+            require_once "login.php";
         ?>
-        <div class="contenedor">
-                
-                <!--<header>
-                    <ul class="nav">
-                        <li> <a href="index.html">Inicio</a></li>
-
-                        <li><a href="aseguradoras.html">Aseguradora</a>
-                            <ul>
-                                <li><a href="">Nueva Aseguradora</a></li>
-                                <li><a href="">Mostrar Aseguradoras</a></li>
-                            </ul>
-                        </li>
-
-                        <li><a href="">Asegurado</a>
-                            <ul>
-                                <li><a href="">Nuevo Asegurado</a></li>
-                                <li><a href="">Mostrar Asegurados</a></li>
-                            </ul>
-                        </li>
-
-                        <li><a href="">Averias</a>
-                            <ul>
-                                <li><a href="">Nueva Averia</a></li>
-                                <li><a href="">Mostrar Averias</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </header>-->
-        </div>
     </body>
 </html>
