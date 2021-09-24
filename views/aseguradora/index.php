de98ca07565bd07213f11a77cf20f3d7a314ec43
pepeju1995 <pepeju95@gmail.com>
2021-09-24 18:57:14 +0200

    cambios formulario aseguradora

--- views/aseguradora/index.php
+++ views/aseguradora/index.php
@@ -13,33 +13,37 @@
         <h1>Aseguradoras</h1>
 
         <form action="<?php echo constant('URL'); ?>aseguradora/nuevaAseguradora" method="post">
-            <p>
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre"  required>
-            </p>
-            <p>
-                <label for="domicilio">Domicilio</label>
-                <input type="text" name="domicilio" required>
-            </p>
-            <p>
-                <label for="cif">CIF</label>
-                <input type="text" name="cif" required>
-            </p>
-            <p>
-                <label for="telefono">Telefono</label>
-                <input type="tel" name="telefono" required>
-            </p>
-            <p>
-                <label for="email">Email</label>
-                <input type="email" name="email" required>
-            </p>
-            <p>
-                <label for="persContacto">Persona de Contacto</label>
-                <input type="text" name="persContacto" required>
-            </p>
-            <p>
+                <div>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre"  required>
+
+                    <label for="cif">CIF</label>
+                    <input type="text" name="cif" required>
+                </div>
+            
+                <div>    
+                    <label for="direccion">Direccion</label>
+                    <input type="text" name="direccion" required>
+
+                    <label for="localidad">Localidad</label>
+                    <input type="text" name="localidad" required>
+                    
+                    <label for="codigopostal">Codigo Postal</label>
+                    <input type="text" name="codigopostal" required>
+                </div>
+                
+                <div>
+                    <label for="telefono">Telefono</label>
+                    <input type="tel" name="telefono" required>
+                            
+                    <label for="email">Email</label>
+                    <input type="email" name="email" required>
+                            
+                    <label for="persContacto">Persona de Contacto</label>
+                    <input type="text" name="persContacto" required>
+                </div>
+                        
                 <input type="submit" value="Registrar nuevo alumno">
-            </p>
         </form>
 
         <?php require_once 'views/footer.php'?>
