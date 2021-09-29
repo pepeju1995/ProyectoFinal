81a98985c4bcc640bbf8cf300d2efc42158f98be
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-09-29 08:17:07 +0200

    Creando tabla asegurados y relaciones

--- config/config.php
+++ config/config.php
@@ -8,8 +8,13 @@ define('USER', 'root');
 define('PASSWORD', '');
 define('CHARSET', 'utf8');
 
+define('TABLAADMIN', 'CREATE TABLE IF NOT EXISTS administrador (id int(4) NOT NULL PRIMARY KEY AUTO_INCREMENT, user varchar(9) NOT NULL UNIQUE, pass varchar(200))');
+
 define('TABLAASEGURADORA', 'CREATE TABLE IF NOT EXISTS aseguradoras (nombre varchar(40), CIF varchar(9) NOT NULL UNIQUE PRIMARY KEY, direccion varchar(50), localidad varchar(30), cp int(5),
 telefono int(9), email varchar(50) UNIQUE, contacto varchar(30))');
 
-define('TABLAUSUARIOS', 'CREATE TABLE IF NOT EXISTS usuarios (id int(4) NOT NULL PRIMARY KEY AUTO_INCREMENT, user varchar(9) UNIQUE, pass varchar(200), rol varchar(15));');
+define('TABLAUSUARIOS', 'CREATE TABLE IF NOT EXISTS usuarios (id int(4) NOT NULL PRIMARY KEY AUTO_INCREMENT, user varchar(9) NOT NULL UNIQUE, pass varchar(200), FOREIGN KEY (user) REFERENCES aseguradoras(cif) ON DELETE CASCADE);');
+
+define('TABLAASEGURADOS', 'CREATE TABLE IF NOT EXISTS asegurados (id int(4) NOT NULL PRIMARY KEY AUTO_INCREMENT, nombre varchar(40), apellido varchar(40), direccion varchar(50), localidad varchar(30), cp int(5),
+telefono int(9), direccion_rep varchar(50), localidad_rep varchar(20), cp_rep varchar(5), aseguradora varchar(9), FOREIGN KEY (aseguradora) REFERENCES aseguradoras(cif))');
 ?>
--- models/loginModel.php
+++ models/loginModel.php
@@ -4,18 +4,27 @@ class LoginModel extends Model{
     function __construct(){
         parent::__construct();
         $query = $this->db->connect();
+        $query->query(constant('TABLAADMIN'));
+        $query->query(constant('TABLAASEGURADORA'));
         $query->query(constant('TABLAUSUARIOS'));
+        $query->query(constant('TABLAASEGURADOS'));
         $clave_admin = md5('admin');
-        $crear_admin = "INSERT INTO usuarios (user, pass, rol) VALUES ('admin', '$clave_admin', 'admin');";
+        $crear_admin = "INSERT INTO administrador (user, pass) VALUES ('admin', '$clave_admin');";
         $query->query($crear_admin);
     }
 
     function login($user, $pass){
         $query = $this->db->connect();
         $password = md5($pass);
-        $stmt = $query->prepare('SELECT * FROM usuarios WHERE user=? AND pass=?');
-        $stmt->bind_param('ss', $user, $password);
-        $stmt->execute();
+        if($user == 'admin'){
+            $stmt = $query->prepare('SELECT * FROM administrador WHERE user=? AND pass=?');
+            $stmt->bind_param('ss', $user, $password);
+            $stmt->execute();
+        } else {
+            $stmt = $query->prepare('SELECT * FROM usuarios WHERE user=? AND pass=?');
+            $stmt->bind_param('ss', $user, $password);
+            $stmt->execute();
+        }
         $result = $stmt->get_result()->fetch_row();
 
         if(count($result)){
