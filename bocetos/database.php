2623f4d55f1e6d624aecdf19b0eaff76d5983606
pepeju1995 <pepeju95@gmail.com>
2021-10-16 09:40:37 +0200

    Borrando estilos anteriores

--- bocetos/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    return array(
-        "host" => "localhost",
-        "user" => "root",
-        "pass" => "",
-        "database" => "solucionesIntegrales",
-        "charset" => "utf8"
-    );
-?>
