6ed4953f5c1698ab07891f6e67ecd4c69bf4e401
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2021-10-18 08:09:31 +0200

    login style

--- views/header.php
+++ views/header.php
@@ -1,20 +1,10 @@
-<header class="row">
-    <?php if(isset($_SESSION['user'])){?>
-        <div class="col-12">
-            <a href="<?php echo constant('URL'); ?>login/cerrarSesion">LogOut</a>
-        </div>
-    <?php }?>
-    <div class="col-12">
-        <div class="row justify-content-center">
-            <img class="col-4" src="<?php echo constant('URL'); ?>public/img/logo.jpg" alt="logotipo">
-            <h1 class="col-6 align-self-center">Soluciones Integrales S.L.</h1>
-        </div>
-    </div>
-
-    <?php if(isset($_SESSION['user'])){?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a href="<?php echo constant('URL'); ?>" class="navbar-brand">
+                <img src="<?php echo constant('URL'); ?>public/img/logo.jpg" alt="logotipo" width="30" class="d-inline-block align-text-top">
+                Soluciones Integrales
+            </a>
+            <?php if(isset($_SESSION['user'])){?>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -23,10 +13,9 @@
                     <a class="nav-link" href="<?php echo constant('URL'); ?>aseguradoras/verAseguradoras">Aseguradora</a>
                     <a class="nav-link" href="<?php echo constant('URL'); ?>asegurados/verAsegurados">Asegurado</a>
                     <a class="nav-link" href="<?php echo constant('URL'); ?>averias/verAverias">Averias</a>
+                    <a class="nav-link" href="<?php echo constant('URL'); ?>login/cerrarSesion">LogOut</a>
                 </div>
             </div>
+            <?php }?>
         </div>
-    </nav>
-        
-    <?php }?>
-</header>
+</nav>
--- views/login/index.php
+++ views/login/index.php
@@ -4,24 +4,35 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo constant('URL'); ?>public/css/bootstrap.min.css">
         <title>Document</title>
     </head>
 
     <body>
-        <?php require_once 'views/header.php'?>
-
-        <div class="contenedor">
-            <h1>Inicio de sesión</h1>
-            <form action="<?php echo constant('URL'); ?>login/iniciarSesion" method="post">
-                <input type="text" name="usuario" placeholder="Usuario">
-                                
-                <input type="password" name="contraseña" placeholder="Contraseña">
-                                
-                <button type="submit">Iniciar Sesion</button>
+        <div class="container">
+            <?php require_once 'views/header.php'?>
+            
+            <form class="form" action="<?php echo constant('URL'); ?>login/iniciarSesion" method="post">
+                <div class="row  justify-content-center">
+                    <div class="col-8 mb-3">
+                        <input class="form-control" type="text" name="usuario" placeholder="Usuario">
+                    </div>                    
+                    <div class="col-8 mb-3">
+                        <input class="form-control" type="password" name="contraseña" placeholder="Contraseña">
+                    </div>          
+                    <div class="col-5 mb-3 text-center">
+                        <button class="btn btn-primary" type="submit">Iniciar Sesion</button>
+                    </div>
+                    
+                </div>
+                
             </form>
+            
+            <?php require 'views/footer.php'?>
         </div>
 
-        <?php require 'views/footer.php'?>
+        <script src="<?php echo constant('URL'); ?>public/js/jquery-3.6.0.js"></script>
+        <script src="<?php echo constant('URL'); ?>public/js/popper.min.js"></script>
+        <script src="<?php echo constant('URL'); ?>public/js/bootstrap.min.js"></script>
     </body>
 </html>
